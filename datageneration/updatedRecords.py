cd879b7dd4bd561a5b09c14eee60bcebd89f2168
Vidyadeor <vidya.deore@clairvoyantsoft.com>
2022-11-16 13:18:25 +0530

    Update updatedRecords.py

--- datageneration/updatedRecords.py
+++ datageneration/updatedRecords.py
@@ -1,57 +1,60 @@
+import configparser
 from pyspark.sql import SparkSession
 from datetime import datetime
+import sys
 
+cfg = configparser.ConfigParser()
+cfg.read('config.ini')
+
+mysql_user = cfg['mysql']['user']
+mysql_pwd_key = cfg['mysql']['pwd_key']
+mysql_password = cfg['mysql']['password']
+
+# password decryption
+def decrypt_value(key, value):
+    try:
+        fernet = Fernet(bytes(key, encoding='utf-8'))
+        decrypted_value = fernet.decrypt(bytes(value, encoding='utf-8')).decode()
+    except Exception as e:
+        print('error while decryption')
+        print(e)
+    return decrypted_value
 
 def create_spark_session():
-    spark = SparkSession \
-        .builder \
-        .appName("Word Count") \
-        .config("spark.driver.extraClassPath", "mysql-connector-java-8.0.23.jar") \
-        .getOrCreate()
+    spark = SparkSession\
+            .builder\
+            .appName("Record_Count")\
+            .getOrCreate()
     return spark
-
-
-def read_from_mysql(table_name):
-    df = spark.read \
-        .format("jdbc") \
-        .option("url", "jdbc:mysql://localhost:3306/peraview") \
-        .option("driver", "com.mysql.jdbc.Driver") \
-        .option("dbtable", table_name) \
-        .option("user", "root") \
-        .option("password", "root123") \
-        .load()
-    return df
-
-
-# df.show(10)
-
+def read_from_mysql(query):
+        password = decrypt_value(mysql_pwd_key, mysql_password)
+        df = spark.read \
+            .format("jdbc") \
+            .option("url", "jdbc:mysql://database-1.chptuk37dacd.ap-south-1.rds.amazonaws.com:3306/pearview") \
+            .option("driver", "com.mysql.jdbc.Driver") \
+            .option("dbtable", query) \
+            .option("user", mysql_user) \
+            .option("password", password) \
+            .load()
+        return  df
+def date_check(dt):
+    try:
+        date_format = '%Y-%m-%d %H:%M:%S'
+        check = datetime.strptime(dt, date_format)
+    except ValueError:
+        print(f"Incorrect START/END data format, should be YYYY-MM-DD HH:MM:MM")
+        raise ValueError(f"Incorrect START/END data format, should be YYYY-MM-DD HH:MM:MM")
 
 if __name__ == "__main__":
-    # table_list = ["student","courses","exam","score"]
-    spark = create_spark_session()
-    df = read_from_mysql("student")
-    # df.show(10)
-    df.createOrReplaceTempView('records')
-    # data = spark.sql(f"select * from records")
-
-
-data = spark.sql(f"select * from records where UPDATED_DATE between '2022-11-03 08:32:23' and '2022-11-06 05:56:25'")
-data.show(10)
-
-start_time = str(input("enter start date:"))
-end_time = str(input("enter end date:"))
-date_format = '%Y-%m-%d %H:%M:%S'
-
-# try:
-# start_date = datetime.strptime(start_time, date_format)
-# except ValueError:
-# print("Incorrect start data format, should be YYYY-MM-DD HH:MM:MM")
-
-# try:
-# end_date = datetime.strptime(end_time, date_format)
-# except ValueError:
-# print("Incorrect end data format, should be YYYY-MM-DD HH:MM:SS")
-# data = spark.sql("select * from records where UPDATED_DATE between '{start_date}' and '{end_date}'")
-data.show(10)
-
-# 2022-11-03 08:32:23
+    #table_list = ["student","courses","exam","score"]
+     start_time = sys.argv[1]
+     end_time = sys.argv[2]
+     date_check(start_time)
+     date_check(end_time)
+    #print(sys.argv)
+     spark = create_spark_session()
+     table_list = ["student"]
+     for table in table_list:
+        query = f"(select * from {table} where UPDATED_DATE between '{start_time}' and '{end_time}') tbl"
+        data = read_from_mysql(query)
+        data.show()
